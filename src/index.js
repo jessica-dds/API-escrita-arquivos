00223c03370968f4528179de23051794903467f4
jessica-dds <jessica.gleizer@hotmail.com>
2024-03-04 18:22:24 -0300

    try, catch e finally

--- src/index.js
+++ src/index.js
@@ -15,15 +15,23 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     const { nome, idade } = req.body;
 
-    const teste = await fs.readFile('./src/usuarios.json');
+    try {
+        const teste = await fs.readFile('./src/usuarios.json');
 
-    const pessoas = JSON.parse(teste);
+        const pessoas = JSON.parse(teste);
 
-    pessoas.push({ nome, idade });
+        pessoas.push({ nome, idade });
 
-    const pessoasStringify = JSON.stringify(pessoas);
+        const pessoasStringify = JSON.stringify(pessoas);
 
-    await fs.writeFile('./src/usuarios.json', pessoasStringify)
+        await fs.writeFile('./src/usuarios.json', pessoasStringify)
+
+
+    } catch (erro) {
+        return res.json(`Ocorreu um erro: ${erro.message}`)
+    } finally {
+        console.log('Isso sempre será executado após o try ou catch');
+    }
 
     return res.json('Pessoa cadastrada com sucesso.')
 });
